6b58c6e1db3515a79a35fd7aa2df8a01040b28c9
June2022 <100213558+June2022@users.noreply.github.com>
2022-04-18 22:11:51 +0800

    Add files via upload

---
+++ practice  code/Day  3.c
@@ -0,0 +1,54 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//int main()//主函数-程序的入口，不能没有main函数，有且仅有一个。
+////int是整型，main前面的int 表示main函数调用返回一个整型数值。
+//{
+//	//这里完成任务
+//	//库函数-C语言本身提供给我们使用的函数。
+//	//printf("Hello world!\n");
+//	char ch = 'A';
+//	printf("%c\n", ch);
+//	return 0;//返回0
+//}
+//#include <stdio.h> main()
+//int main()
+//{
+//	int i, j;
+//	i = 010;
+//	j = 9;
+//	printf("%d", i);
+//	return 0;
+//}
+/*/#include <stdio.h>
+#include <string.h>
+int main()
+{
+	printf("%d\n", strlen("c:\test\32\test.c"));//\32作为8进制代表的那个十进制数字作为ASII码对应的字符
+	//printf("%c\n", '\'');
+	//printf("%s\n","\"");
+	//printf("c:\\test\\32\\test.c");
+	//char arr1[] = "abc";
+	//char arr2[] = { 'a','b','c' };
+	//printf("%d\n", strlen(arr2));//strlen-string length 计算字符串的长度
+	return 0;
+}*/
+//#include <stdio.h>
+//int main() {
+	/*int line=0;
+	printf("好好学C语言：\n");
+	while (line < 2000)
+	{
+		printf("敲一行代码%d\n",line);
+		line++;
+	}
+	printf("好offer");*/
+	/*int putin = 0;
+	printf("上大学\n");
+	printf("你要好好学习吗（0/1）>:");
+	scanf("%d", &putin);
+	if (putin == 1)
+		printf("好offer\n");
+	else
+		printf("被淘汰\n");*/
+	//return 0;
+//}
